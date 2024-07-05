286808047da00036c87bcb10d16e5dbe68f68cb4
valentino <collazo.valentino@gmail.com>
2024-07-05 10:29:39 -0300

    Small Suck implementaion

--- src/main_scenes/player/player.gd
+++ src/main_scenes/player/player.gd
@@ -5,7 +5,8 @@ enum States {IDLE, WALK, JUMP, HURT, SUCK, DUCK, FALL}
 
 @export var health = 8
 
-var _state: States = States.JUMP 
+var _state: States = States.JUMP
+var _shouldSuck : bool 
 
 @onready var _sprite = $AnimatedSprite2D
 @onready var _movementController = $MovementController
@@ -15,16 +16,27 @@ func _ready():
 	_movementController.set_controlled_owner(self)
 	
 func _physics_process(delta):
-	print(_state)
+	if _state == States.SUCK:
+		velocity = Vector2.ZERO
 	_do_animation()
 	move_and_slide()
 	_set_states()
 
+
 func _set_states() -> void:
+	if _shouldSuck:
+		_state = States.SUCK
+		return
 	if velocity.x == 0 and is_on_floor():
 		_state = States.IDLE
 	if velocity.x != 0 and is_on_floor():
 		_state = States.WALK
+
+func _unhandled_input(event):
+	if Input.is_action_just_pressed("shoot") and is_on_floor():
+		_shouldSuck = true
+	if Input.is_action_just_released("shoot") and is_on_floor():
+		_shouldSuck = false
 
 func _do_animation() -> void:
 	if velocity.x > 0: 
